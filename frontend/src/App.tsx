97564261b242c773bdab69d30b5dfc49df7c2ee2
Justin Tyson <61070079+okjustin@users.noreply.github.com>
2025-11-05 19:55:59 +0000

    Add project colours to tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-3xl font-bold underline text-primary-500 bg-primary-900">
         Hello!
       </h1>
     </>
