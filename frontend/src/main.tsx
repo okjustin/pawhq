259140a070ba31b2fa330cd63bab235c71db3009
Justin Tyson <61070079+okjustin@users.noreply.github.com>
2025-11-06 02:27:04 +0000

    Fix query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { startMirage } from './lib/mirage'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
 import './styles/styles.css'
 
 startMirage()
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>
 )
