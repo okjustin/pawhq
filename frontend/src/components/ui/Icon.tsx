4e5b3e4ec182be53b895d93ea2aace39db95c0ec
Justin Tyson <61070079+okjustin@users.noreply.github.com>
2025-11-06 14:35:35 +0000

    Update icon SVGs

--- frontend/src/components/ui/Icon.tsx
+++ frontend/src/components/ui/Icon.tsx
@@ -5,31 +5,47 @@ type Props = {
 
 const icons = {
   error: (
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-    />
+    <>
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 6L18 18M6 18L18 6"
+      />
+    </>
   ),
   warning: (
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-    />
+    <>
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 6v8"
+      />
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 18h.01"
+      />
+    </>
   ),
   info: (
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-    />
+    <>
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 6h.01"
+      />
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 12v6"
+      />
+    </>
   ),
   success: (
     <path
       strokeLinecap="round"
       strokeLinejoin="round"
-      d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+      d="M5 13l4 4L19 7"
     />
   )
 }
@@ -48,9 +64,9 @@ const circleSizes = {
 }
 
 const svgSizes = {
-  sm: 'w-3 h-3',
-  md: 'w-4 h-4',
-  lg: 'w-5 h-5'
+  sm: 'w-6 h-6',
+  md: 'w-8 h-8',
+  lg: 'w-10 h-10'
 }
 
 export default function Icon({ type, size = 'md' }: Props) {
@@ -60,7 +76,7 @@ export default function Icon({ type, size = 'md' }: Props) {
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
-        strokeWidth={2}
+        strokeWidth={3}
         stroke="currentColor"
         className={svgSizes[size]}
       >
