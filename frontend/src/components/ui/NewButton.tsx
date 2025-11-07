e7c2e31f6675754287f0d9dffea101683c8f3d0e
Justin Tyson <61070079+okjustin@users.noreply.github.com>
2025-11-07 14:51:03 +0000

    NewButton component formatting

--- frontend/src/components/ui/NewButton.tsx
+++ frontend/src/components/ui/NewButton.tsx
@@ -10,20 +10,30 @@ export default function NewButton() {
       label='New Item Menu'
     >
       <div>
-        <Link to='/pets/new' className='flex items-center w-full px-3 py-2 hover:bg-gray-100 rounded'>
+        <Link
+          to='/pets/new'
+          className='flex items-center w-full px-3 py-2 hover:bg-gray-100 rounded'
+        >
           <FaPaw className='mr-2 text-lg text-text-secondary' /> New Pet
         </Link>
 
         <hr className='my-2 border-t border-gray-300' />
 
-        <Link to='/clients/new' className='flex items-center w-full px-3 py-2 hover:bg-gray-100 rounded'>
+        <Link
+          to='/clients/new'
+          className='flex items-center w-full px-3 py-2 hover:bg-gray-100 rounded'
+        >
           <FaPerson className='mr-2 text-lg text-text-secondary' /> New Client
         </Link>
 
         <hr className='my-2 border-t border-gray-300' />
 
-        <Link to='/appointments/new' className='flex items-center w-full px-3 py-2 hover:bg-gray-100 rounded'>
-          <FaCalendar className='mr-2 text-lg text-text-secondary' /> New Appointment
+        <Link
+          to='/appointments/new'
+          className='flex items-center w-full px-3 py-2 hover:bg-gray-100 rounded'
+        >
+          <FaCalendar className='mr-2 text-lg text-text-secondary' /> New
+          Appointment
         </Link>
       </div>
     </Popover>
